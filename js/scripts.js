94cf6f9d27c0ab3215b7aba281b034f12f5c02b2
Ted B <tedabickham@hotmail.com>
2014-11-20 10:19:22 -0800

    Version 0.02

--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,16 @@ $(document).ready(function(){
 	$('body').css('background', '#0A7020');
 	$('body > div').css('background', '#A8A8A8');
 	$('body > div').css('border', 'solid 0.5em #029030');
+	$('body > div').css('position', 'absolute');
+	$('body > div').css('top', '10em');
+	$('body > div').css('display', 'inline');
 	$('a:link').css('color', 'blue');
 	$('a.visited').css('color', 'purple');
+	$('body > p').css('font-size', '48px');
+	$('body > p').css('color', '#1B8131');
 
 	//$('h1 > span').css("transform: rotateX(" + (Math.rand(30) - 15) + ");");
-	$('h1.blast-root .blast:nth-child(odd)').css('font-size', ((Math.random() * 48) + 32) + 'px');
-	$('p.blast-root .blast:nth-child(even)').css('font-size', ((Math.random() * 2) + 12) + 'px');
-	$('a.blast-root .blast:nth-child(even)').css('font-size', ((Math.random() * 2) + 12) + 'px');
+	$('h1.blast-root .blast:nth-child(odd)').css('font-size', ((Math.random() * 12) + 32) + 'px');
+	$('p.blast-root .blast:nth-child(even)').css('font-size', ((Math.random() * 4) + 12) + 'px');
+	$('a.blast-root .blast:nth-child(even)').css('font-size', (Math.random() + 12) + 'px');
 });
